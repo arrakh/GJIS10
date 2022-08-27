494b529ba2289760a6674fcc6b67dbefd9853c28
Arya Rakha <arrakhpra@gmail.com>
2022-08-27 17:56:08 +0700

    New Scenes

--- Assets/Scripts/Entities/EndGoal.cs
+++ Assets/Scripts/Entities/EndGoal.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using Player;
+using UnityEngine;
 
 namespace Entities
 {
     public class EndGoal : MonoBehaviour
     {
-        
+        public Action onGoal;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent(out PlayerController player)) return;
+            
+            onGoal?.Invoke();
+        }
     }
 }
